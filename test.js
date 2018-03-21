a50b1b90af564531735c864fe0572266e1c88604
pavansachi <pavan8sachi@gmail.com>
2018-03-21 22:12:46 +1100

    add new test

--- test.js
+++ test.js
@@ -3,9 +3,14 @@ const mocha = require('mocha');
 const calc = require('./calculator');
 
 describe('Calculator', function() {
-  describe('add()', function() {
+  describe('add positive numbers()', function() {
     it('should return 10 when the value is 5 and 5', function() {
       assert.equal(calc(5,5), 10);
+    });
+  });
+  describe('add negative numbers()', function() {
+    it('should return -5 when the value is -2 and -3', function() {
+      assert.equal(calc(-2,-3), -6);
     });
   });
 });
